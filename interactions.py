c9b6650df6fc65dd8843bcdabbdcfe2b946af968
Jin Zhao <jzhao567@uwo.ca>
2025-05-07 23:06:14 -0600

    testing

--- interactions.py
+++ interactions.py
@@ -1,5 +1,14 @@
 # THIS FINALLY WORKED OMG FINALLY YES
-import utils as utils
+# import utils
+import requests
+from html.parser import HTMLParser
+import json
+from address_parser import AddressParser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
 
 # Set up Chrome (headless optional)
 options = Options()
