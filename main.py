4332929b4eaf8f33b6801064183b098f4cc49bd7
Jin Zhao <jzhao567@uwo.ca>
2025-08-26 22:28:19 -0600

    chkpt

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from notion_client import Client
 from notion_actions import create_notion_entry
 from get_page import get_pages
 
-NOTION_TOKEN = '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'
-DATABASE_ID = '1e8ccd11787580e1a575c6ade25b0efd'
 
 def main():
     url = "https://offcampus.uwo.ca/Listings/"
